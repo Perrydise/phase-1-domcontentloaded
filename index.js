b11abcb34e4cecf49d93e8bd3253ef35870e43a2
Perry Jorss <pjorss123@gmail.com>
2022-05-03 17:12:15 -0400

    commit tree 66df29df7f0589449220ebfe8630ad4915e3de1e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  let editThis =  document.getElementById("text")
+  console.log(editThis.textContent)
+  let toThis = "This is really cool!"
+  editThis.textContent = toThis
+    }
+) 
